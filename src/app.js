9b4380e44cf9b96d07f0109bd26a5fef7cd7e405
gmodel95 <gisellemodel95@gmail.com>
2023-09-01 19:22:39 +1000

    Search function input

--- src/app.js
+++ src/app.js
@@ -44,8 +44,17 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "89d392c4e269069d1257947e5fb1a4c4";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "89d392c4e269069d1257947e5fb1a4c4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Milan");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
